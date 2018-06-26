604035dbf01881b00c81b2dc2dddfd2e419094ca
Arthur <agcruatem@hotmail.com>
2018-06-26 14:13:01 -0300

    * Centraliza "+" de botão de adicionar cards * Diminui fonte de títulos de listas * Deixa linhas da lista "Fontes de dados" com localização baseada em porcentagem * Aumenta altura da lista "Matriz de influências"

--- src/app/components/List/List.jsx
+++ src/app/components/List/List.jsx
@@ -38,7 +38,7 @@ class List extends Component {
                                 cards={list.cards}
                                 boardId={boardId}
                             />
-                            <div className={boardId=="DOWN RIGHT"?(index==2||index==3?'cards-wrapper list-card-small':'cards-wrapper'):(boardId=="DOWN LEFT"?(index!=0?'cards-wrapper list-card-'+(index==1||index==2?'small':'ultra-small'):'cards-wrapper'):'cards-wrapper')}>
+                            <div className={boardId=="DOWN RIGHT"?(index==2||index==3?'cards-wrapper list-card-down-right':'cards-wrapper'):(boardId=="DOWN LEFT"?(index!=0?'cards-wrapper list-card-'+(index==1||index==2?'down-left-available':'down-left-missing'):'cards-wrapper'):'cards-wrapper')}>
                                 <Cards listId={list._id} index={index} boardId={boardId} listTitle={list.title}/>
                             </div>
                         </div>
